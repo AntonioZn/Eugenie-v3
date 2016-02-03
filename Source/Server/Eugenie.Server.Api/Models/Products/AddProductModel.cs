064aad11aac46895d76ebb9fbd3f91aca1eb8583
Svetlozar Stoichkov <sv.stoichkov@yahoo.com>
2016-02-03 23:29:38 +0200

    Revert deletion of AddProductModel

---
+++ Source/Server/Eugenie.Server.Api/Models/Products/AddProductModel.cs
@@ -0,0 +1,45 @@
+﻿namespace Eugenie.Server.Api.Models.Products
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text.RegularExpressions;
+
+    using Common.Constants;
+
+    using Data.Models;
+
+    public class AddProductModel
+    {
+        private string name;
+
+        [Required]
+        [MaxLength(ValidationConstants.ProductNameMaxLength)]
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                this.name = Regex.Replace(value.Trim(), @"\s+", " ");
+            }
+        }
+
+        [MaxLength(ValidationConstants.ProductNameMaxLength)]
+        public string OldName { get; set; }
+
+        public decimal BuyingPrice { get; set; }
+
+        public decimal? SellingPrice { get; set; }
+
+        public MeasureType Measure { get; set; }
+
+        public decimal? QuantityToAdd { get; set; }
+
+        public ICollection<Barcode> Barcodes { get; set; }
+
+        public ICollection<ExpirationDate> ExpirationDates { get; set; }
+    }
+}
