cc4090ea6bf930e4ce3b8f0ffaa0c933709eb12c
Svetlozar Stoichkov <sv.stoichkov@yahoo.com>
2016-02-10 23:14:41 +0200

    Seller is automatically started as administrator

--- Source/Clients/Eugenie.Clients.Seller/App.xaml.cs
+++ Source/Clients/Eugenie.Clients.Seller/App.xaml.cs
@@ -1,5 +1,10 @@
 ﻿namespace Eugenie.Clients.Seller
 {
+    using System;
+    using System.Diagnostics;
+    using System.Reflection;
+    using System.Security.Principal;
+    using System.Windows;
     using System.Windows.Threading;
 
     using Common.Helpers;
@@ -10,6 +15,23 @@
     {
         public App()
         {
+            if (!this.IsRunAsAdministrator())
+            {
+                var processInfo = new ProcessStartInfo(Assembly.GetExecutingAssembly().CodeBase);
+                
+                processInfo.UseShellExecute = true;
+                processInfo.Verb = "runas";
+                
+                try
+                {
+                    Process.Start(processInfo);
+                    Application.Current.Shutdown();
+                }
+                catch
+                {
+                }
+            }
+
             this.InitializeComponent();
             this.DispatcherUnhandledException += this.OnUnhandledException;
 
@@ -21,6 +43,14 @@
         {
             Logger.LogToFile(e.Exception, Settings.Default.LogPath);
             e.Handled = true;
+        }
+
+        private bool IsRunAsAdministrator()
+        {
+            var wi = WindowsIdentity.GetCurrent();
+            var wp = new WindowsPrincipal(wi);
+
+            return wp.IsInRole(WindowsBuiltInRole.Administrator);
         }
     }
 }
