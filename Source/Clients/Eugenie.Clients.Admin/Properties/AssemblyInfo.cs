e70728e53dace761f60442f71d614deb369e3714
Svetlozar Stoichkov <sv.stoichkov@yahoo.com>
2016-02-13 01:16:21 +0200

    Versions set to 1.2.0

--- Source/Clients/Eugenie.Clients.Admin/Properties/AssemblyInfo.cs
+++ Source/Clients/Eugenie.Clients.Admin/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyVersion("1.1.3")]
-[assembly: AssemblyFileVersion("1.1.3")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
 
 [assembly: AssemblyTitle("EugenieAdmin")]
 [assembly: AssemblyDescription("")]
--- Source/Clients/Eugenie.Clients.Seller/Properties/AssemblyInfo.cs
+++ Source/Clients/Eugenie.Clients.Seller/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyVersion("1.1.7")]
-[assembly: AssemblyFileVersion("1.1.7")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
 
 [assembly: AssemblyTitle("EugenieSeller")]
 [assembly: AssemblyDescription("")]
