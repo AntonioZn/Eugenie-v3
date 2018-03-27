b87bb6f143e47a5c9dd745de4a02376e10f825dd
Svetlozar Stoichkov <sv.stoichkov@yahoo.com>
2018-03-27 17:10:01 +0300

    Implemented Settings and SettingsManager

---
+++ Source/Clients/Eugenie.Clients.Seller/Helpers/SettingsManager.cs
@@ -0,0 +1,41 @@
+﻿namespace Eugenie.Clients.Seller.Helpers
+{
+    using System;
+    using System.IO;
+
+    using Models;
+
+    using Newtonsoft.Json;
+
+    public static class SettingsManager
+    {
+        private static readonly string settingsFilePath;
+
+        static SettingsManager()
+        {
+            var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var userFilePath = Path.Combine(localAppData, "Eugenie");
+            settingsFilePath = Path.Combine(userFilePath, "SellerSettings.txt"); ;
+
+            Directory.CreateDirectory(userFilePath);
+        }
+
+        public static Settings Get()
+        {
+            if (File.Exists(settingsFilePath))
+            {
+                var settingsString = File.ReadAllText(settingsFilePath);
+                var settings = JsonConvert.DeserializeObject<Settings>(settingsString);
+
+                return settings;
+            }
+
+            return new Settings();
+        }
+
+        public static void Save(Settings settings)
+        {
+            File.WriteAllText(settingsFilePath, JsonConvert.SerializeObject(settings));
+        }
+    }
+}
--- Source/Clients/Eugenie.Clients.Seller/Models/Settings.cs
+++ Source/Clients/Eugenie.Clients.Seller/Models/Settings.cs
@@ -1,69 +1,25 @@
 ﻿namespace Eugenie.Clients.Seller.Models
 {
-    using System;
-    using System.IO;
-
-    using Newtonsoft.Json;
-
     public class Settings
     {
-        private readonly string settingsFilePath;
-
-        public Settings()
-        {
-            var localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            var userFilePath = Path.Combine(localAppData, "Eugenie");
-            this.settingsFilePath = Path.Combine(userFilePath, "SellerSettings.txt"); ;
-
-            Directory.CreateDirectory(userFilePath);
-        }
-
-        public void Load()
-        {
-            if (File.Exists(this.settingsFilePath))
-            {
-                var settingsString = File.ReadAllText(this.settingsFilePath);
-                var settings = JsonConvert.DeserializeObject<Settings>(settingsString);
-
-                this.Address = settings.Address;
-                this.LogPath = settings.LogPath;
-                this.ReceiptPath = settings.ReceiptPath;
-                this.IsSelfHost = settings.IsSelfHost;
-                this.Port = settings.Port;
-                this.BackupDatabase = settings.BackupDatabase;
-                this.BackupHours = settings.BackupHours;
-                this.BackupMinutes = settings.BackupMinutes;
-                this.BackupPath = settings.BackupPath;
-                this.LotteryUsername = settings.LotteryUsername;
-                this.LotteryPassword = settings.LotteryPassword;
-            }
-        }
-
-        public string Address { get; set; } = "";
-
-        public string LogPath { get; set; } = "C:\\EugenieLogs";
-
-        public string ReceiptPath { get; set; } = "C:\\EugenieReceipts";
-
-        public bool IsSelfHost { get; set; }
+        public bool IsSelfHost { get; set; } = true;
 
         public int Port { get; set; } = 9000;
 
-        public bool BackupDatabase { get; set; }
+        public bool BackupDatabase { get; set; } = true;
 
-        public int BackupHours { get; set; } = 20;
+        public string BackupPath { get; set; } = "C:\\EugenieBackups";
+
+        public int BackupHours { get; set; } = 21;
 
         public int BackupMinutes { get; set; } = 0;
 
-        public string BackupPath { get; set; } = "C:\\EugenieBackups";
+        public string ServerAddress { get; set; }
+
+        public string ReceiptPath { get; set; } = "C:\\EugenieReceipts";
 
         public string LotteryUsername { get; set; }
 
         public string LotteryPassword { get; set; }
-
-        public void Save()
-        {
-            File.WriteAllText(this.settingsFilePath, JsonConvert.SerializeObject(this));
-        }
     }
 }
